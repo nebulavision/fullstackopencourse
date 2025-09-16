e275af81187e03371ca5956b4304772da0331cfd
Bruno Díaz <brunodiazmartin5@gmail.com>
2025-09-16 23:53:42 +0100

    End of 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props) => {
+  return <h1>{ props.course }</h1>
+} 
 
-function App() {
-  const [count, setCount] = useState(0)
+const Content = (props) => {
+  return (
+    <div>
+      {
+        props.content.map(item => 
+          <Part 
+            part={ item.part } 
+            exercises={ item.exercises } 
+          />
+        )           
+      }
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return <p>{ props.part } { props.exercises }</p>
+}
+
+const Total = (props) => {
+  return <p>Number of exercises { props.total }</p>
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  
+  const content = [
+    {
+      part: 'Fundamentals of React',
+      exercises: 10    
+    },
+    {
+      part: 'Using props to pass data',
+      exercises: 7  
+    },
+    {
+      part: 'State of a component',
+      exercises: 14 
+    }
+  ]
+
+  const totalExcercises = content.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course={ course } />
+      <Content content={ content }/>
+      <Total total={ totalExcercises } />
+    </div>
   )
 }
 
