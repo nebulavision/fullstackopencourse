8be91bd1277278f5f2e1e49bffab47ad53d5c3e2
Bruno Díaz <brunodiazmartin5@gmail.com>
2025-09-19 15:44:36 +0100

    End of 2.20 excercise

--- part2/countries/src/services/weatherService.js
+++ part2/countries/src/services/weatherService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = '4e0cf67417824f8eaeb135218251909'
+const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL= `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=[countryName]`
 
 const getWeather = (countryName) => {
